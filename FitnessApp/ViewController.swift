51ff5ca4fe50c536b1ee65b1d370aff0a0cf16b7
Daniel Banach <djbanach@comcast.net>
2019-06-28 16:53:15 -0700

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Daniel Banach on 6/28/19.
+//  Copyright © 2019 Daniel Banach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
